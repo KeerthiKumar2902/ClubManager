b5bda6dc7ab4e3f74e0c2bd060d18e2167df3613
KeerthiKumar2902 <keerthikumarkk1@gmail.com>
2026-01-13 15:37:49 +0530

    Initial commit: Project setup, database connected, basic server running

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight">
+        Keerthi Kumar S
+      </h1>
+    </>
+  )
+}
+
+export default App
