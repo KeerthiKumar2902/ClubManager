a0ca247d65a333488d74823484132ba63590a7d2
KeerthiKumar2902 <keerthikumarkk1@gmail.com>
2026-03-01 00:46:55 +0530

    fxing cors rules

--- server/index.js
+++ server/index.js
@@ -14,7 +14,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Allow Frontend to talk to Backend
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+); // Allow Frontend to talk to Backend
 app.use(express.json()); // Parse incoming JSON data
 
 // Routes
