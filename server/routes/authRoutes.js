c513f6431cc3745f332ae3400a0dd73bac98e13a
KeerthiKumar2902 <keerthikumarkk1@gmail.com>
2026-03-01 01:19:19 +0530

    google call back fix

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-const authMiddleware = require('../middleware/authMiddleware');
-const { authLimiter } = require('../middleware/rateLimiter');
+const authMiddleware = require("../middleware/authMiddleware");
+const { authLimiter } = require("../middleware/rateLimiter");
 const passport = require("passport");
-const jwt = require("jsonwebtoken"); 
+const jwt = require("jsonwebtoken");
 const upload = require("../middleware/upload");
 
 // Rate Limited Public Routes
@@ -19,18 +19,26 @@ router.post("/reset-password", authLimiter, authController.resetPassword);
 router.post("/verify-email", authController.verifyEmail);
 
 // Protected Routes
-router.put("/profile", authMiddleware,upload.single("avatar"), authController.updateProfile);
+router.put(
+  "/profile",
+  authMiddleware,
+  upload.single("avatar"),
+  authController.updateProfile,
+);
 
 // 1. Redirect to Google
 router.get(
   "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
+  passport.authenticate("google", { scope: ["profile", "email"] }),
 );
 
 // 2. Google Callback
 router.get(
   "/google/callback",
-  passport.authenticate("google", { session: false, failureRedirect: "/login" }),
+  passport.authenticate("google", {
+    session: false,
+    failureRedirect: "/login",
+  }),
   (req, res) => {
     // This runs ONLY if login successful
     const user = req.user;
@@ -39,13 +47,16 @@ router.get(
     const token = jwt.sign(
       { id: user.id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: "1d" }
+      { expiresIn: "1d" },
     );
 
+    const frontendUrl = process.env.FRONTEND_URL || "http://localhost:5173";
+
     // Redirect to Frontend with Token
-    // We pass the token in the URL (Frontend will grab it)
-    res.redirect(`http://localhost:5173/google-callback?token=${token}&user=${encodeURIComponent(JSON.stringify(user))}`);
-  }
+    res.redirect(
+      `${frontendUrl}/google-callback?token=${token}&user=${encodeURIComponent(JSON.stringify(user))}`,
+    );
+  },
 );
 
 module.exports = router;
